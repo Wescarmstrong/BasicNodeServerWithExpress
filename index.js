93fbc8a8c35ba61cb85cdca85c005b968c5da8fa
Wes <wescarmstrong@gmail.com>
2021-11-05 22:28:27 -0500

    Barebone Node and express

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
-// const http = require('http')
-// const fs = require('fs')
-// const homePage = fs.readFileSync('index.html')
-// const aboutPage = fs.readFileSync('about.html')
-// const contactPage = fs.readFileSync('contact.html')
-// const notFoundPage = fs.readFileSync('notfound.html')
-
-// const server = http.createServer((req, res) =>{
-//     if(req.url === '/about')
-//         res.end(aboutPage)
-//     else if(req.url === '/contact')
-//         res.end(contactPage)
-//     else if(req.url === '/')
-//         res.end(homePage)
-//     else {
-//         res.writeHead(404)
-//         res.end(notFoundPage)
-//     }
-// })
-
-// server.listen(3000)
+app.use(express.static('public'))
 
 
 app.listen(3000, () => {
